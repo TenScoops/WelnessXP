87198aa7aaeef9467c83d8cb7d29112262edc617
TenScoops <p.ayres9@gmail.com>
2024-02-29 15:37:35 -0500

    icon addition

--- Frontend/src/Components/Navbar/Navbar.tsx
+++ Frontend/src/Components/Navbar/Navbar.tsx
@@ -1,11 +1,11 @@
-import { Heart2, Settings } from "../Icons/Icons"
+import { Heart2, Settings, RedirectIcon } from "../Icons/Icons"
 
 const Navbar = () => {
     return(
     <div className=" w-full fixed bg-slate-50 z-50">
        <div className=" flex flex-row justify-between items-center">
             <div className="ml-5">
-                <h1>Life XP</h1>
+                <h3>Life XP</h3>
                 <div className="flex flex-row items-center">
                     <h3 className="text-xs underline mr-1">
                         Wellness
@@ -14,9 +14,14 @@ const Navbar = () => {
                 </div>
             </div>
             <div className="flex flex-row space-x-14 text-sm">
-                <h1 className="cursor-pointer">About</h1>
-                <h1 className="cursor-pointer">Features</h1>
-                <h1 className="cursor-pointer">LifeXP</h1>
+                <h3 className="cursor-pointer">About</h3>
+                <h3 className="cursor-pointer">Features</h3>
+                <div className="flex items-center space-x-1">
+                    <h3 className="cursor-pointer">
+                        LifeXP
+                    </h3>
+                    <RedirectIcon />
+                </div>
             </div>
             <button className="mr-5 text-sm">
                 Logout
